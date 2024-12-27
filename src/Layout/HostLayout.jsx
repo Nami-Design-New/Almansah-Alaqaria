161136a8fad77d99a2576207184e67c42c46f239
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-12-27 17:08:01 +0200

    fix

--- src/Layout/HostLayout.jsx
+++ src/Layout/HostLayout.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../ui/Footer";
 import Header from "../ui/Header";
+import AuthModal from "../ui/modals/AuthModal";
 
 export default function HostLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Header isHost={true} />
@@ -10,6 +18,7 @@ export default function HostLayout() {
         <Outlet />
       </main>
       <Footer />
+      <AuthModal />
     </>
   );
 }
--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "../ui/Header";
 import Footer from "./../ui/Footer";
 import AuthModal from "../ui/modals/AuthModal";
 
 export default function RootLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Header />
